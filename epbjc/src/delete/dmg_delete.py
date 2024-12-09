0c4717d329e9bc24213a6ffd4411d6c52fed2737
Sol3904 <solanumofthequantummoon@gmail.com>
2024-12-09 10:18:18 +0000

    msm coisa nas lanes
    update igual ao anterior mas com feito no lanes

--- epbjc/src/delete/dmg_delete.py
+++ epbjc/src/delete/dmg_delete.py
@@ -3,7 +3,7 @@ import sqlite3
 conex = sqlite3.connect('C:\\Users\\solan\\Desktop\\epbjc\\epbjc\\sqlite-database\\epbjc.db')
 cursor_dmg = conex.cursor()
 
-apagar_nome = input("Nengue diz me o nome ")
+apagar_nome = input("Diga me o nome da linha a apagar ")
 
 cursor_dmg.execute('DELETE FROM champs_dmg WHERE nome = ?',(apagar_nome,))
 
--- epbjc/src/delete/lanes_delete.py
+++ epbjc/src/delete/lanes_delete.py
@@ -1,1 +1,20 @@
- 
+import sqlite3
+
+conex = sqlite3.connect('C:\\Users\\solan\\Desktop\\epbjc\\epbjc\\sqlite-database\\epbjc.db')
+cursor_lanes = conex.cursor()
+
+pergunta = input("Quer apagar a tabela champs_lanes? ")
+if pergunta == "sim":
+    crtz = input("Tem a certeza? ")
+    if crtz == "sim":
+        absoluta = input("Tem a certeza absoluta? ")
+        if absoluta == "sim":
+            warning = input("Cuidado que vai tudo á vida, quer MESMO? ")
+            if warning == "sim":
+                cursor_lanes.execute('DELETE FROM champs_lanes')
+
+if pergunta or crtz or absoluta or warning == "nao":
+    print("Obrigada meu")
+
+conex.commit()
+conex.close()
